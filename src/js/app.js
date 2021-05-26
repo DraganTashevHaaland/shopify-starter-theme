25a67cd8f9a5be12e49751270a06da42fd6871b4
DraganTashevHaaland <60400101+DraganTashevHaaland@users.noreply.github.com>
2021-05-26 18:15:06 +0200

    fdsf

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,8 @@
-
-
-// Lazy loading
-import 'lazysizes';
-// import a plugin
-import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-
 import Vue from "vue";
 window.Vue = Vue;
 
 let axios = require('axios');
-window.axios = axios;
-
+window.Vue = Vue;
 
 let jQuery = require('jquery');
 window.jQuery = window.$ = jQuery;
@@ -19,21 +11,8 @@ import 'popper.js';
 
 import 'bootstrap';
 
-window.Noty = require('noty');
-
-
-// Vue custom filter
-require('./filters/money.js');
-
-
-// Vue Components
-
 require('./components/ProductForm.js');
-require('./components/CartForm.js');
-require('./components/MiniCart.js');
 
 
-// javascript
-require('./product.js');
-require('./header.js');
-
+import ('./header');
+import ('./product');
--- src/js/product.js
+++ src/js/product.js
@@ -1,25 +1,24 @@
 
-let slick = require('slick-carousel');
+let slick = require('slick-carousel'); 
 
 $(".product-slider").slick({
  
     // normal options...
-    infinite: true,
-    arrows:false,
-    slidesToShow: 1,
-    asNavFor: '.product-slider-nav'
-   
-});
+        infinite: true,
+        arrows: false,
+        slidesToShow: 1,
+        asNavFor: '.prdict-slider-nav'
+          
+        
+  });
 
-$(".product-slider-nav").slick({
- 
-    // normal options...
-    infinite: true,
-    arrows:false,
-    slidesToShow: 3,
-    asNavFor: '.product-slider',
-    slidesToScroll: 1,
-    centerMode: true,
-    focusOnSelect: true
-   
-});
+  $("product-slider-nav").slick({
+
+        infinite: true,
+        arrows: false,
+        slidesToShow: 3,
+        asNavFor: 'product-slider',
+        slidesToScroll: 1,
+        centerMode: true,
+        focusOnSelect: true
+  });
